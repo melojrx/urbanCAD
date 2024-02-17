c98613055cbba86ef53ca748086a681c1bdb6ebc
Cyro Sabóia <cyrosaboia@gmail.com>
2024-02-17 11:06:29 -0300

    Restaurando database.py

---
+++ app/database.py
@@ -0,0 +1,15 @@
+from app import app
+from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
+
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://cad_maceio:cad1q2w3emaceio@10.82.85.8:19000/urban_maceio'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/postgres'
+
+app.config['SECRET_KEY'] = 'secret'
+
+db = SQLAlchemy(app)
+
+@app.errorhandler(Exception)
+def internal_error(e):
+    db.session.rollback()
+    return render_template('erro.html', e=e)
