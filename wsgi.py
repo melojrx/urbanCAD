897cdc421a6e0d843824e226f4f8f3fed7cb6cc9
Cyro Sabóia <cyrosaboia@gmail.com>
2024-02-23 15:26:40 -0300

    Ajustando arquivos para o server

--- wsgi.py
+++ wsgi.py
@@ -3,4 +3,4 @@ from app import app, socketio
 if __name__ == "__main__":
         # app.run(host='0.0.0.0', port=8009)
         socketio.run(app, debug=True)
-        # socketio.run(app, debug=True, cors_allowed_origins='*', ping_timeout=30)
+        # socketio.run(app, debug=True, cors_allowed_origins='http://201.47.23.87:8009/', ping_timeout=90)
